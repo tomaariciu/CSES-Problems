c17f0863b816f1d3a80fa4ea54f58cb351bb4289
Toma Ariciu <tomaariciu@gmail.com>
2024-09-20 10:52:31 +0300

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+/// My current template for cpp solutions
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+void solve() {
+
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+#endif // LOCAL
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int tests = 1;
+    //cin >> tests;
+    while (tests--) {
+        solve();
+    }
+    return 0;
+}
