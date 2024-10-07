b4817def0ada3da71267833cc6b6742bda9da740
Toma Ariciu <tomaariciu@gmail.com>
2024-10-07 10:27:41 +0300

    Changed indentation to spaces

--- Introductory Problems/Weird Algorithm.cpp
+++ Introductory Problems/Weird Algorithm.cpp
@@ -5,17 +5,17 @@ using ll = long long;
 using ld = long double;
 
 void solve() {
-	ll n;
-	cin >> n;
-	cout << n << ' ';
-	while(n != 1) {
-		if (n % 2 == 1) {
-			n = 3 * n + 1;
-		} else {
-			n = n / 2;
-		}
-		cout << n << ' ';
-	}
+    ll n;
+    cin >> n;
+    cout << n << ' ';
+    while(n != 1) {
+        if (n % 2 == 1) {
+            n = 3 * n + 1;
+        } else {
+            n = n / 2;
+        }
+        cout << n << ' ';
+    }
 }
 
 int main() {
